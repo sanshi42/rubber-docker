ed6140244fdd362f4c4126759432c8aff98b796b
sanshi <sanshizhisu@outlook.com>
2025-05-29 22:03:02 +0800

    feat: 实现 Level 00

--- levels/00_fork_exec/rd.py
+++ levels/00_fork_exec/rd.py
@@ -14,10 +14,10 @@ Usage:
 """
 
 
-
-import click
 import os
 import traceback
+
+import click
 
 
 @click.group()
@@ -31,15 +31,17 @@ def contain(command):
     # NOTE: command is an array (the first element is path/file, and the entire
     #       array is exec's args)
 
-    os._exit(0)  # TODO: remove this after adding exec
+    os.execv(command[0], command)
 
 
-@cli.command(context_settings=dict(ignore_unknown_options=True,))
+@cli.command(
+    context_settings=dict(
+        ignore_unknown_options=True,
+    )
+)
 @click.argument('Command', required=True, nargs=-1)
 def run(command):
-    # TODO: replace this with fork()
-    #       (https://docs.python.org/2/library/os.html#os.fork)
-    pid = 0
+    pid = os.fork()
     if pid == 0:
         # This is the child, we'll try to do some containment here
         try:
@@ -47,11 +49,11 @@ def run(command):
         except Exception:
             traceback.print_exc()
             os._exit(1)  # something went wrong in contain()
-
-    # This is the parent, pid contains the PID of the forked process
-    # wait for the forked child and fetch the exit status
-    _, status = os.waitpid(pid, 0)
-    print('{} exited with status {}'.format(pid, status))
+    else:
+        # This is the parent, pid contains the PID of the forked process
+        # wait for the forked child and fetch the exit status
+        _, status = os.waitpid(pid, 0)
+        print('{} exited with status {}'.format(pid, status))
 
 
 if __name__ == '__main__':
